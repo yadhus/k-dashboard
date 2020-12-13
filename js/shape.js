37ffc00da3be2896d672f79a521beff1b2109760
Yadhunandan S <yadhus@users.noreply.github.com>
2020-12-13 13:42:56 +0530

    random sound updated

--- js/shape.js
+++ js/shape.js
@@ -1,135 +1,85 @@
-let keyData = {
-  q: {
-    sound: new Howl({
-      src: ["./assets/sounds/bubbles.mp3"],
-    }),
-  },
-  w: {
-    sound: new Howl({
-      src: ["./assets/sounds/clay.mp3"],
-    }),
-  },
-  e: {
-    sound: new Howl({
-      src: ["./assets/sounds/confetti.mp3"],
-    }),
-  },
-  r: {
-    sound: new Howl({
-      src: ["./assets/sounds/corona.mp3"],
-    }),
-  },
-  t: {
-    sound: new Howl({
-      src: ["./assets/sounds/dotted-spiral.mp3"],
-    }),
-  },
-  y: {
-    sound: new Howl({
-      src: ["./assets/sounds/flash-1.mp3"],
-    }),
-  },
-  u: {
-    sound: new Howl({
-      src: ["./assets/sounds/flash-2.mp3"],
-    }),
-  },
-  i: {
-    sound: new Howl({
-      src: ["./assets/sounds/flash-3.mp3"],
-    }),
-  },
-  o: {
-    sound: new Howl({
-      src: ["./assets/sounds/glimmer.mp3"],
-    }),
-  },
-  p: {
-    sound: new Howl({
-      src: ["./assets/sounds/moon.mp3"],
-    }),
-  },
-  a: {
-    sound: new Howl({
-      src: ["./assets/sounds/pinwheel.mp3"],
-    }),
-  },
-  s: {
-    sound: new Howl({
-      src: ["./assets/sounds/piston-1.mp3"],
-    }),
-  },
-  d: {
-    sound: new Howl({
-      src: ["./assets/sounds/piston-2.mp3"],
-    }),
-  },
-  f: {
-    sound: new Howl({
-      src: ["./assets/sounds/prism-1.mp3"],
-    }),
-  },
-  g: {
-    sound: new Howl({
-      src: ["./assets/sounds/prism-2.mp3"],
-    }),
-  },
-  h: {
-    sound: new Howl({
-      src: ["./assets/sounds/prism-3.mp3"],
-    }),
-  },
-  j: {
-    sound: new Howl({
-      src: ["./assets/sounds/splits.mp3"],
-    }),
-  },
-  k: {
-    sound: new Howl({
-      src: ["./assets/sounds/squiggle.mp3"],
-    }),
-  },
-  l: {
-    sound: new Howl({
-      src: ["./assets/sounds/strike.mp3"],
-    }),
-  },
-  z: {
-    sound: new Howl({
-      src: ["./assets/sounds/suspension.mp3"],
-    }),
-  },
-  x: {
-    sound: new Howl({
-      src: ["./assets/sounds/timer.mp3"],
-    }),
-  },
-  c: {
-    sound: new Howl({
-      src: ["./assets/sounds/ufo.mp3"],
-    }),
-  },
-  v: {
-    sound: new Howl({
-      src: ["./assets/sounds/veil.mp3"],
-    }),
-  },
-  b: {
-    sound: new Howl({
-      src: ["./assets/sounds/wipe.mp3"],
-    }),
-  },
-  n: {
-    sound: new Howl({
-      src: ["./assets/sounds/zig-zag.mp3"],
-    }),
-  },
-  m: {
-    sound: new Howl({
-      src: ["./assets/sounds/moon.mp3"],
-    }),
-  },
-};
+let soundData = [
+  new Howl({
+    src: ["./assets/sounds/bubbles.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/clay.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/confetti.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/corona.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/dotted-spiral.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/flash-1.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/flash-2.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/flash-3.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/glimmer.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/moon.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/pinwheel.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/piston-1.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/piston-2.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/prism-1.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/prism-2.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/prism-3.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/splits.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/squiggle.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/strike.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/suspension.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/timer.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/ufo.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/veil.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/wipe.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/zig-zag.mp3"],
+  }),
+  new Howl({
+    src: ["./assets/sounds/moon.mp3"],
+  }),
+];
+
+let keyData = "qwertyuiopasdfghjklzxcvbnm";
 
 document.addEventListener("keydown", onKeyDown);
 
@@ -153,7 +103,7 @@ function getRandomColor() {
 }
 
 function onKeyDown(event) {
-  if (keyData[event.key]) {
+  if (keyData.indexOf(event.key) !== -1) {
     let randomYPosition = y + parseInt(Math.random() * height);
     let randomXPosition = x + parseInt(Math.random() * width);
     let randomRadius = 100 + parseInt(Math.random() * 200);
@@ -171,7 +121,9 @@ function onKeyDown(event) {
 
     circleContainer.append(circle);
 
-    keyData[event.key].sound.play();
+    let randomIndex = Math.floor(Math.random() * 26);
+
+    soundData[randomIndex].play();
 
     setTimeout(() => {
       circle.style.opacity = "0";
